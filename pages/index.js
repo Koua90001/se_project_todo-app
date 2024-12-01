c2a19a70174f51cda5bf95e03d05c337cffce9d0
KKO <oumarkouakou@yahoo.com>
2024-12-01 02:07:33 -0500

    sprint8

--- pages/index.js
+++ pages/index.js
@@ -3,93 +3,84 @@ console.log(uuidv4());
 import { initialTodos, validationConfig } from "../utils/constants.js"; 
 import Todo from "../components/Todo.js";
 import formValidator from '../components/FormValidator.js';
-
+import Section from '../components/section.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import TodoCounter from '../components/TodoCounter.js';
  
 
 const addTodoButton = document.querySelector(".button_action_add");
-const addTodoPopup = document.querySelector("#add-todo-popup");
-const addTodoForm = addTodoPopup.querySelector(".popup__form");
-const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-const todoTemplate = document.querySelector("#todo-template");
-const todosList = document.querySelector(".todos__list");
+const addTodoPopupEl = document.querySelector("#add-todo-popup");
+const addTodoForm = addTodoPopupEl.querySelector(".popup__form");
+// const addTodoCloseBtn = addTodoPopupEl.querySelector(".popup__close");
+// const todosList = document.querySelector(".todos__list");
 
-const openModal = (modal) => {
-  modal.classList.add("popup_visible");
-};
+const todoCounter = new TodoCounter(initialTodos, ".counter__text");
 
-const closeModal = (modal) => {
-  modal.classList.remove("popup_visible");
-};
+const addTodoPopup = new PopupWithForm({
+  popupSelector: "#add-todo-popup",
+  handleFormSubmit: (inputValues) => {
+    const name = inputValues.name;
+    const dateInput = inputValues.date;
 
-// The logic in this function should all be handled in the Todo class.
+    const date = new Date(dateInput);
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+
+    const id = uuidv4();
+    const values = { name, date, id };
+    const todo = generateTodo(values);
+    section.addItem(todo);
+    addTodoPopup.close();
+    newTodoValidator.resetValidation();
+    todoCounter.updateTotal(true);
+    // console.log(inputValues.target.name.value);
+    // console.log(inputValues.target.date.value);
+  },
+});
+addTodoPopup.setEventListeners();
+
+function handleCheck(completed) {
+  todoCounter.updateCompleted(completed);
+}
+
+function handleDelete(completed) {
+  if (completed) {
+    todoCounter.updateCompleted(false);
+  }
+  todoCounter.updateTotal(false);
+}
+
+function handleTotal(completed) {
+  if (completed) {
+    todoCounter.updateTotal(false);
+  }
+}
+
 const generateTodo = (data) => {
-  const todo = new Todo(data, "#todo-template");
+  const todo = new Todo(
+    data,
+    "#todo-template",
+    handleCheck,
+    handleDelete,
+    handleTotal
+  );
   const todoElement = todo.getView();
   return todoElement;
+};
 
- // const todoElement = todoTemplate.content  
- //   .querySelector(".todo")
-// .cloneNode(true);
-//  const todoNameEl = todoElement.querySelector(".todo__name");
-//  const todoCheckboxEl = todoElement.querySelector(".todo__completed");
-//  const todoLabel = todoElement.querySelector(".todo__label");
-//  const todoDate = todoElement.querySelector(".todo__date");
-//  const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
+const section = new Section({
+  items: initialTodos,
+  renderer: (data) => {
+    const todo = generateTodo(data);
+    section.addItem(todo);
+  },
+  containerSelector: ".todos__list",
+});
 
-//  todoNameEl.textContent = data.name;
-//  todoCheckboxEl.checked = data.completed;
-
-  // Apply id and for attributes.
-  // The id will initially be undefined for new todos.
-//  todoCheckboxEl.id = `todo-${data.id}`;
-//  todoLabel.setAttribute("for", `todo-${data.id}`);
-
-  // If a due date has been set, parsing this it with `new Date` will return a
-  // number. If so, we display a string version of the due date in the todo.
-//  const dueDate = new Date(data.date);
-//  if (!isNaN(dueDate)) {
-//    todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
-//      year: "numeric",
-//      month: "short",
-//      day: "numeric",
- //   })}`;
-
-  }
-
-//  todoDeleteBtn.addEventListener("click", () => {
-//    todoElement.remove();
-//  });
-
-  
-//};
+section.renderItems();
 
 addTodoButton.addEventListener("click", () => {
-  openModal(addTodoPopup);
+  addTodoPopup.open();
 });
 
-addTodoCloseBtn.addEventListener("click", () => {
-  closeModal(addTodoPopup);
-});
-
-addTodoForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const name = evt.target.name.value;
-  const dateInput = evt.target.date.value;
-
-  // Create a date object and adjust for timezone
-  const date = new Date(dateInput);
-  date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-
-  const id = uuidv4();
-  const values = { name, date, id };
-  const todo = generateTodo(values);
-  todosList.append(todo);
-  closeModal(addTodoPopup);
-});
-
-initialTodos.forEach((item) => {
-  const todo = generateTodo(item);
-  todosList.append(todo);
-});
 const newTodoValidator = new formValidator(validationConfig, addTodoForm);
 newTodoValidator.enableValidation();
